172f72b17e261bb2993b025a4b74135d4aac5db8
jerry67 <liulooong@126com>
2014-11-04 20:23:08 +0800

    somehtml

--- 1.php
+++ 1.php
@@ -1,2 +1,12 @@
-<?php
-	echo "hello github!";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>我的第一个github页面</h1>
+    <p>这个是我在github上创建的第一个页面，你好，github。</p>
+    <img src="./img/lin.jpg" alt="linChiling"/>
+</body>
+</html>
