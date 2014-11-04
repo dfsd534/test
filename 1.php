0df99c2464e5c8547a37cc9f4f2984688cdfe954
jerry67 <liulooong@126com>
2014-11-04 20:34:00 +0800

    add some css

--- 1.php
+++ 1.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        hear{width: 400px;margin: 0 auto;border: 1px solid #ccc;padding 10px;}
+        h1,p,img {margin:0 auto;}
+    </style>
 </head>
 <body>
-    <h1>我的第一个github页面</h1>
-    <p>这个是我在github上创建的第一个页面，你好，github。</p>
-    <img src="./img/lin.jpg" alt="linChiling"/>
+    <div id="header">
+        <h1>我的第一个github页面</h1>
+        <p>这个是我在github上创建的第一个页面，你好，github。</p>
+        <img src="./img/lin.jpg" alt="linChiling"/>
+    </div>
 </body>
 </html>
